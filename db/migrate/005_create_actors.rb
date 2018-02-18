a9b6909fc7a61254e69e97d85fae2afbd3df2b14
Kevin Huang <yellow.khaos@gmail.com>
2018-02-18 21:46:44 +0000

    automatically backed up by learn

--- db/migrate/005_create_actors.rb
+++ db/migrate/005_create_actors.rb
@@ -1,5 +1,11 @@
 class CreateActors
+  has_many :characters
+  has_many :shows, through: :characters
+  
   def change
-
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
   end
 end
