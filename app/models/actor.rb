ec39aece659d5c6f64014c327278bf6c0c51c5bc
Kevin Huang <yellow.khaos@gmail.com>
2018-02-18 22:37:41 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     self.characters.collect do |character|
-      "#{character.name} - #{character.show}"
+      "#{character.name} - #{character.show.name}"
     end
   end
 end
