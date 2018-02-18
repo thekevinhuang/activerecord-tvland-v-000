f08147a2e02533678eb57bb8c3409000f15c1124
Kevin Huang <yellow.khaos@gmail.com>
2018-02-18 21:51:23 +0000

    automatically backed up by learn

--- db/migrate/004_create_characters.rb
+++ db/migrate/004_create_characters.rb
@@ -1,11 +1,11 @@
 class CreateCharacters
   belongs_to :actor
   belongs_to :show
-  
+
   def change
     create_table :characters do |t|
       t.string :catchphrase
-      
+
     end
   end
 end
