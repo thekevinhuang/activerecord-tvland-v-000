611be77afc4dc4eef2e29962a8e470998c422ea4
Kevin Huang <yellow.khaos@gmail.com>
2018-02-18 22:37:01 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,10 @@ class Actor < ActiveRecord::Base
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show}"
+    end
+  end
 end
